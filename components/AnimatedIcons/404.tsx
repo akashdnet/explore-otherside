8a67caa0b82d2acccc3c5b65dd61c4b773022723
akashdnet <khondokerakashmi@gmail.com>
2025-12-22 01:08:22 +0600

    - 404 page added - loading page added

--- components/AnimatedIcons/404.tsx
+++ components/AnimatedIcons/404.tsx
@@ -1,9 +1,9 @@
 "use client";
-import Icon from "@/public/lotties/loading-icon.json";
+import Icon from "@/public/lotties/404.json";
 import Lottie from "lottie-react";
 
-const NotFoundAnimation = () => (
-    <span className="flex justify-center items-center rounded-2xl ">
+const SignUpAnimation = () => (
+    <span className="flex justify-center items-center rounded-2xl w-full">
         <Lottie
             animationData={Icon}
             loop={true}
@@ -12,4 +12,4 @@ const NotFoundAnimation = () => (
     </span>
 );
 
-export default NotFoundAnimation;
+export default SignUpAnimation;
